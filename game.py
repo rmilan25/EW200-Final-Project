07966bc38d242cccfd354ba88ff2be966d4dc000
rmilan25 <m254362@usna.edu>
2022-11-10 09:46:20 -0500

    revised tank code, removed turret

--- game.py
+++ game.py
@@ -7,15 +7,13 @@ screen_rect = screen.get_rect() #get rect attributes of screen
 
 screen.fill((0,0,0)) #paint the surface background
 
-turret = pygame.image.load('images/turret.png')
-turret_rect = turret.get_rect()
-screen.blit(turret, (200-2, 400-40-30))
-
 tank = pygame.image.load('images/tank.png') #load image
 tank_rect = tank.get_rect() #get rect attributes of tank
-screen.blit(tank,(200-30,400-40))
+tank_rect.midbottom = screen_rect.midbottom
+screen.blit(tank,tank_rect)
 
 pygame.display.flip()
+
 while True:
     for event in pygame.event.get(): #listen for events
         if event.type == pygame.QUIT: #if window is closed, game quits
